4c477f9d7f82c36c03fc28ab3a0ceafff396556d
ALLAN BIGHI BUENO <allanbbueno@bol.com.br>
2022-10-02 18:26:01 -0300

    update 001

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+
+const app = express()
+
+app.listen('3000')
+
+
+let author = "Allan Bueno"
+
+//middleware
+app.use(express.json())
+
+//app.route('/').post((req, res)=>res.send(req.body))
+
+
+
+// app.route('/').get( (req,res) => res.send(author))
+
+// app.route('/').put((req, res)=>{
+//     author = req.body.author
+//     res.send(author)
+// })
+
+
+// Rqeuisição DELETE
+// app.route('/:identificador').delete( (req,res) => {
+    // res.send(req.params.identificador)})
+
+
+app.route('/').get( (req,res) => res.send(req.query.name))
+app.route('/').put( (req,res) => res.send(req.query.author))
+app.route('/').get( (req,res) => res.send(req.params.parametro))
