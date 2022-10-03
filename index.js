3358af2c991a22e506c0cc24aa610f24f35b1067
ALLAN BIGHI BUENO <allanbbueno@bol.com.br>
2022-10-03 14:02:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,23 +10,25 @@ let author = "Allan Bueno"
 //middleware
 app.use(express.json())
 
-//app.route('/').post((req, res)=>res.send(req.body))
+app.route('/').post((req, res)=>res.send(req.body))
 
 
 
-// app.route('/').get( (req,res) => res.send(author))
+app.route('/').get( (req,res) => res.send(author))
 
-// app.route('/').put((req, res)=>{
-//     author = req.body.author
-//     res.send(author)
-// })
+app.route('/').put((req, res)=>{
+    author = req.body.author
+    res.send(author)
+})
 
 
 // Rqeuisição DELETE
-// app.route('/:identificador').delete( (req,res) => {
-    // res.send(req.params.identificador)})
+app.route('/:identificador').delete( (req,res) => {
+    res.send(req.params.identificador)})
 
 
 app.route('/').get( (req,res) => res.send(req.query.name))
 app.route('/').put( (req,res) => res.send(req.query.author))
 app.route('/').get( (req,res) => res.send(req.params.parametro))
+
+app.route('/').get( (req,res) => res.send("Olá"))
